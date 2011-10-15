45ca9da3cd1f21261108f3f5303b4a381bd7ef17
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-10-15 21:05:10 +0200

    restore ruby 1.8 compatibility

--- lib/blasphemy.rb
+++ lib/blasphemy.rb
@@ -87,7 +87,11 @@ module Faker
         "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee."
       ]
       def paragraph
-        @paragraph_list.sample
+        if @paragraph_list.respond_to?(:sample)
+          @paragraph_list.sample
+        else
+          @paragraph_list[rand(@paragraph_list.size)]
+        end
       end
 
       def paragraphs(count)
