8d7349df12b2ffe26f20632b986855fb9fc9f336
Michał Krzyżanowski <michal.krzyzanowski+github@gmail.com>
2011-10-15 17:35:43 +0200

    small code cleanup

--- lib/blasphemy.rb
+++ lib/blasphemy.rb
@@ -53,7 +53,6 @@ module Faker
     end
   end
 
-
   class BaconIpsum < CustomIpsum
     def initialize
       @wordlist = [
@@ -87,9 +86,8 @@ module Faker
         "Well, the way they make shows is, they make one show. That show's called a pilot. Then they show that show to the people who make shows, and on the strength of that one show they decide if they're going to make more shows. Some pilots get picked and become television programs. Some don't, become nothing. She starred in one of the ones that became nothing.",
         "The path of the righteous man is beset on all sides by the iniquities of the selfish and the tyranny of evil men. Blessed is he who, in the name of charity and good will, shepherds the weak through the valley of darkness, for he is truly his brother's keeper and the finder of lost children. And I will strike down upon thee with great vengeance and furious anger those who would attempt to poison and destroy My brothers. And you will know My name is the Lord when I lay My vengeance upon thee."
       ]
-
       def paragraph
-        @paragraph_list[rand(@paragraph_list.size)]
+        @paragraph_list.sample
       end
 
       def paragraphs(count)
